5bff67f3bcbf3807254cd093f076fdbca5a1d814
Johan Böhlin <johboh@gmail.com>
2025-05-01 15:11:28 +0200

    Add packing

--- src/Ieee802154NetworkShared.h
+++ src/Ieee802154NetworkShared.h
@@ -28,14 +28,14 @@ struct __attribute__((packed)) MessageV1 {
 /**
  * Sent by nodes to perform host discovery.
  */
-struct DiscoveryRequestV1 {
+struct __attribute__((packed)) DiscoveryRequestV1 {
   uint8_t id = MESSAGE_ID_DISCOVERY_REQUEST_V1;
 };
 
 /**
  * Sent by host to confirm host discovery.
  */
-struct DiscoveryResponseV1 {
+struct __attribute__((packed)) DiscoveryResponseV1 {
   uint8_t id = MESSAGE_ID_DISCOVERY_RESPONSE_V1;
   uint8_t channel; // 802.15.4 channel to use when sending messages. Must be a valid 802.15.4 channel between 11 and 26.
 };
@@ -43,7 +43,7 @@ struct DiscoveryResponseV1 {
 /**
  * Sent by host to node for time syncronization.
  */
-struct PendingTimestampResponseV1 {
+struct __attribute__((packed)) PendingTimestampResponseV1 {
   uint8_t id = MESSAGE_ID_PENDING_TIMESTAMP_RESPONSE_V1;
   uint64_t timestamp; // unix timestamp in seconds, UTC.
 };
@@ -52,7 +52,7 @@ struct PendingTimestampResponseV1 {
  * Sent by host when node has been informed about pending messages.
  * This message allows for the host to provide additional application specific payload.
  */
-struct PendingPayloadResponseV1 {
+struct __attribute__((packed)) PendingPayloadResponseV1 {
   uint8_t id = MESSAGE_ID_PENDING_PAYLOAD_RESPONSE_V1;
   uint8_t payload[]; // We know the size as we know the size from the decrypted outer message. Max size 79 bytes.
 };
@@ -62,7 +62,7 @@ struct PendingPayloadResponseV1 {
  * This is part 1/3 of the firmware update process. We cannot fit everyting in one message.
  * This can tecnically be omitted if the node already knows about the wifi credentials.
  */
-struct PendingFirmwareWifiCredentialsResponseV1 {
+struct __attribute__((packed)) PendingFirmwareWifiCredentialsResponseV1 {
   uint8_t id = MESSAGE_ID_PENDING_FIRMWARE_WIFI_CREDENTIALS_RESPONSE_V1;
   char wifi_ssid[32];     // WiFi SSID that node should connect to.
   char wifi_password[32]; // WiFi password that the node should connect to.
@@ -74,7 +74,7 @@ struct PendingFirmwareWifiCredentialsResponseV1 {
  * Sent by host when node has been informed about pending messages.
  * This is part 2/3 of the firmware update process. We cannot fit everyting in one message.
  */
-struct PendingFirmwareChecksumResponseV1 {
+struct __attribute__((packed)) PendingFirmwareChecksumResponseV1 {
   uint8_t id = MESSAGE_ID_PENDING_FIRMWARE_CHECKSUM_RESPONSE_V1;
   char md5[32];        // MD5 hash of firmware. Does not include trailing \0
   uint32_t identifier; // Set by host. All firmware update will have the same ID, so node should verify that they are
@@ -86,7 +86,7 @@ struct PendingFirmwareChecksumResponseV1 {
  * This is part 3/3 of the firmware update process. We cannot fit everyting in one message.
  * After this message, firmware update will proceed, given that wifi credentials have been proided earlier.
  */
-struct PendingFirmwareResponseV1 {
+struct __attribute__((packed)) PendingFirmwareResponseV1 {
   uint8_t id = MESSAGE_ID_PENDING_FIRMWARE_URL_RESPONSE_V1;
   char url[75];        // url where to find firmware binary. Note the max file path.
   uint32_t identifier; // Set by host. All firmware update will have the same ID, so node should verify that they are
